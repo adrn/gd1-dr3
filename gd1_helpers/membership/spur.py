dba4b56e53311478f76b6fcfb8cad91a7719f508
Adrian Price-Whelan <adrian.prw@gmail.com>
2022-10-05 18:22:01 -0400

    stronger prior

--- gd1_helpers/membership/spur.py
+++ gd1_helpers/membership/spur.py
@@ -60,7 +60,7 @@ class SpurModel(StreamModel):
             ln_normal(
                 pars["mean_phi2"],
                 1.3 * jax.nn.sigmoid((cls.phi2_knots - -39) / 2.5),
-                3.0,
+                0.5,
             )
         )
 
